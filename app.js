c17a11b80b70af6cfbda9789f63b23d6b23e0a00
Djaafar Boumaza <26055564+djaafarcode@users.noreply.github.com>
2017-08-28 17:00:14 +0200

    work in ma space working

---
+++ app.js
@@ -0,0 +1,79 @@
+let express = require('express');
+let app = express();
+let mustache = require('mustache');
+let fs = require('fs');
+let bodyParser = require('body-parser');
+let tabArt = [];
+//Djaafar
+let tabuser = [];
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+let tabEvent = [{
+    name: "test",
+    content: "ceci est un test"
+}];
+
+app.use("/", express.static("public"));
+
+app.post("/event", function(req, res) {
+    console.log(req.body);
+    res.send(req.body);
+});
+
+app.post("/event/add", function(req, res) {
+    let obj = {};
+    obj.title = req.body.title;
+    obj.content = req.body.content;
+    tabArt.push(obj);
+    res.send(tabArt);
+});
+app.post("/event/del", function(req, res) {
+    array.splice(req.body.id, 1);
+    res.send('okay');
+});
+app.get("/", function(req, res) {
+    res.render("index");
+});
+
+// ####### Djaafar #######
+
+//Create a path
+app.post("/user/add", function(req, res) {
+    console.log(req.body);
+    //add tab value of form client.
+    tabuser.push(req.body);
+    console.log(tabuser);
+    res.send(req.body);
+});
+
+//load file adduser on the path.
+app.get("/user/add", function(req, res) {
+    res.render("adduser");
+});
+// ####### Djaafar #######
+
+app.engine("html", function(path, options, callback) {
+    fs.readFile(path, function(err, content) {
+        if (err) {
+            console.error("fail to open template:", err);
+            return callback(err);
+        }
+        let str = mustache.render(content.toString(), options);
+        return callback(null, str);
+    });
+});
+app.listen(9000, function(err) {
+    if (err) {
+        console.log(err);
+        return;
+    };
+    console.log('Server Ok');
+
+});
+
+app.set('views', './template');
+app.set('view engine', "html");
---
+++ public/script.js
@@ -0,0 +1,79 @@
+let title = document.querySelector("#title");
+let content = document.querySelector('#content');
+let btn = document.querySelector('#btn');
+let art = document.querySelector('#art');
+
+btn.addEventListener("click", function(e) {
+    let jsonData = JSON.stringify({ title: title.value, content: content.value });
+    e.preventDefault();
+    fetch("/event", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: jsonData
+    }).then(function(response) {
+        return response.json();
+    }).then(function(obj) {
+        aff(obj);
+    }).catch(function(error) {
+        console.error(error);
+    });
+    title.value = "";
+    content.value = "";
+    document.querySelector("#formcont").style.display = "none";
+
+});
+
+function aff(obj) {
+    let article = document.createElement('article');
+    let h2 = document.createElement('h2');
+    let p = document.createElement('p');
+    let btn = document.createElement('button');
+    let div = document.createElement('div');
+    let hr = document.createElement('hr');
+
+    article.className = "container margb";
+    div.className = "row";
+    h2.className = "col-11";
+    btn.className = "col-1 btn btn-outline-danger btn-sm";
+    hr.className = "col-4 text-center";
+
+    h2.textContent = obj.title;
+    p.textContent = obj.content;
+    btn.textContent = "X";
+
+    btn.addEventListener("click", function() {
+        article.remove();
+    })
+
+    div.appendChild(h2);
+    div.appendChild(btn);
+    article.appendChild(div);
+    article.appendChild(hr);
+    article.appendChild(p);
+    art.appendChild(article);
+}
+
+document.querySelector("#newevent").addEventListener("click", function(e) {
+    e.preventDefault();
+    let form = document.querySelector("#eventForm");
+    document.querySelector("#formcont").style.display = "flex";
+    let overForm = false;
+    form.addEventListener("mouseover", function() {
+        overForm = true;
+    });
+
+    form.addEventListener("mousemove", function() {
+        overForm = true;
+    });
+
+    form.addEventListener("mouseout", function() {
+        overForm = false;
+    });
+    document.querySelector("#formcont").addEventListener("click", function() {
+        if (overForm === false) {
+            document.querySelector("#formcont").style.display = "none";
+            title.value = "";
+            content.value = "";
+        }
+    });
+});
